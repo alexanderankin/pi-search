0bca7db32160e839c17b25fba3ff7f8fd6c1eea2
David Ankin <daveankin@gmail.com>
2022-10-22 13:11:56 -0400

    add test

---
+++ pi-generation/src/test/java/info/ankin/pisearch/generation/PiGeneratorTest.java
@@ -0,0 +1,17 @@
+package info.ankin.pisearch.generation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PiGeneratorTest {
+
+    @Test
+    void test_tenIterations() {
+        Object result = new PiGenerator().calculate(10);
+        System.out.println(result);
+
+        assertEquals(String.valueOf(result), "3.1415926535897936");
+    }
+
+}
