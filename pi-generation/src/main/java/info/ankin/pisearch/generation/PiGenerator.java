71430896488efbde60a758bd6cdfd660047b224e
David Ankin <daveankin@gmail.com>
2022-10-22 14:18:54 -0400

    attempt 1

--- pi-generation/src/main/java/info/ankin/pisearch/generation/PiGenerator.java
+++ pi-generation/src/main/java/info/ankin/pisearch/generation/PiGenerator.java
@@ -1,5 +1,11 @@
 package info.ankin.pisearch.generation;
 
+import ch.obermuhlner.math.big.BigDecimalMath;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.NavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
@@ -7,35 +13,84 @@ import java.util.concurrent.ConcurrentSkipListMap;
  * @see <a href="https://stackoverflow.com/a/46166848">original answer on SO</a>
  */
 public class PiGenerator {
-    private final NavigableMap<Integer, Integer> map = new ConcurrentSkipListMap<>();
+    private static final BigDecimal NEG_ONE = BigDecimal.ONE.negate();
+    private static final BigDecimal BD_3 = new BigDecimal(3);
+    private static final BigDecimal BD_6 = new BigDecimal(6);
+    private static final BigDecimal BD_12 = new BigDecimal(12);
+    private static final BigDecimal BD_3_Over_2 = new BigDecimal("3.0").divide(new BigDecimal(2), RoundingMode.UNNECESSARY);
+    private static final BigDecimal CH_CONST_1 = new BigDecimal(545140134);
+    private static final BigDecimal CH_CONST_2 = new BigDecimal(13591409);
+    private static final Raiseable CH_CONST_3 = new Raiseable(new BigDecimal(640320));
+    private final NavigableMap<BigInteger, BigInteger> map = new ConcurrentSkipListMap<>();
+    private final int precision;
+    private final MathContext mathContext;
 
-    public double calculate(int k) {
-        double result = 0;
+    public PiGenerator() {
+        this(1074); // default double precision
+    }
+
+    public PiGenerator(int precision) {
+        this(new MathContext(precision, RoundingMode.HALF_UP));
+    }
+
+    public PiGenerator(MathContext mathContext) {
+        this.precision = mathContext.getPrecision();
+        this.mathContext = mathContext;
+    }
+
+    public BigDecimal calculate(int k) {
+        BigDecimal result = new BigDecimal(0);
         for (int i = 0; i <= k; i++) {
-            result = result + doCalc(i);
+            result = result.add(doCalc(i));
         }
-        return 1 / (result * 12);
+        return BigDecimal.ONE.divide(result.multiply(BD_12), mathContext);
     }
 
-    double doCalc(int k) {
-        double numerator = Math.pow(-1, k) * factorial(6 * k) * (545140134 * k + 13591409);
-        double denominator = factorial(3 * k) * Math.pow(factorial(k), 3) * Math.pow(640320, 3 * k + 3.0 / 2.0);
-        return numerator / denominator;
+    BigDecimal doCalc(int k) {
+        BigDecimal kBd = new BigDecimal(k);
+        BigDecimal numerator =
+        NEG_ONE.pow(k)
+                .multiply(factorial(6 * k))
+                .multiply(CH_CONST_1.multiply(kBd).add(CH_CONST_2));
+                ;
+
+        BigDecimal denominator = factorial(3 * k)
+                .multiply(factorial(k)).pow(3)
+                .multiply(CH_CONST_3.pow(kBd.multiply(BD_3).add(BD_3_Over_2), mathContext));
+                // Math.pow(640320, 3 * k + 3.0 / 2.0);
+
+        return numerator.divide(denominator, mathContext);
     }
 
-    double factorial(int n) {
-        return intFactorial(n);
+    BigDecimal factorial(int n) {
+        return new BigDecimal(intFactorial(BigInteger.valueOf(n)));
     }
 
-    int intFactorial(int n) {
+    BigInteger intFactorial(BigInteger n) {
         return map.computeIfAbsent(n, this::doFactorial);
     }
 
-    int doFactorial(int n) {
-        if (n == 0) {
-            return 1;
+    BigInteger doFactorial(BigInteger n) {
+        if (n.equals(BigInteger.ZERO)) {
+            return BigInteger.ONE;
         } else {
-            return n * intFactorial(n - 1);
+            return n.multiply(intFactorial(n.subtract(BigInteger.ONE)));
+        }
+    }
+
+    public static class Raiseable {
+        private final BigDecimal base;
+
+        public Raiseable(BigDecimal base) {
+            this.base = base;
+        }
+
+        public BigDecimal getBase() {
+            return base;
+        }
+
+        public BigDecimal pow(BigDecimal exponent, MathContext mathContext) {
+            return BigDecimalMath.pow(base, exponent, mathContext);
         }
     }
 
--- pi-generation/src/test/java/info/ankin/pisearch/generation/PiGeneratorTest.java
+++ pi-generation/src/test/java/info/ankin/pisearch/generation/PiGeneratorTest.java
@@ -2,16 +2,18 @@ package info.ankin.pisearch.generation;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.MathContext;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PiGeneratorTest {
 
     @Test
     void test_tenIterations() {
-        Object result = new PiGenerator().calculate(10);
+        Object result = new PiGenerator(MathContext.DECIMAL128).calculate(10);
         System.out.println(result);
 
-        assertEquals(String.valueOf(result), "3.1415926535897936");
+        assertEquals(String.valueOf(result).substring(0, 17), "3.141592653589793");
     }
 
 }
