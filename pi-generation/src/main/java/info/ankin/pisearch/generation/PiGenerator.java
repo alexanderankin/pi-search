e7eb4258104865df8813879c1a5c78df3f3cedbb
David Ankin <daveankin@gmail.com>
2022-10-22 17:27:12 -0400

    simplify factorial methods, just use bigint

--- pi-generation/src/main/java/info/ankin/pisearch/generation/PiGenerator.java
+++ pi-generation/src/main/java/info/ankin/pisearch/generation/PiGenerator.java
@@ -9,7 +9,6 @@ import java.math.RoundingMode;
 import java.util.Map;
 import java.util.NavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
-import java.util.function.Function;
 
 /**
  * @see <a href="https://stackoverflow.com/a/46166848">original answer on SO</a>
@@ -20,6 +19,7 @@ public class PiGenerator {
      */
     private static final int MAX_DECIMALS_IN_FLOAT = 16;
     private static final BigInteger NEGATIVE_ONE = BigInteger.ONE.negate();
+    private static final BigInteger SIX = BigInteger.valueOf(6);
     /**
      * Optional cache
      */
@@ -65,14 +65,15 @@ public class PiGenerator {
     }
 
     BigDecimal doCalc(int k) {
+        BigInteger kBigInt = BigInteger.valueOf(k);
         BigInteger numerator = (k % 2 == 0 ? BigInteger.ONE : NEGATIVE_ONE)
-                .multiply(factorial(6 * k))
+                .multiply(factorial(kBigInt.multiply(SIX)))
                 .multiply(BigInteger.valueOf(545140134L * k + 13591409));
         BigDecimal d =
                 new BigDecimal(
-                        factorial(BigInteger.valueOf(3).multiply(BigInteger.valueOf(k)).intValue())
+                        factorial(BigInteger.valueOf(3).multiply(kBigInt))
                 )
-                        .multiply(BigDecimalMath.pow(new BigDecimal(factorial(k)),
+                        .multiply(BigDecimalMath.pow(new BigDecimal(factorial(kBigInt)),
                                 new BigDecimal(BigInteger.valueOf(3)),
                                 mathContext))
                         .multiply(BigDecimalMath.pow(new BigDecimal(BigInteger.valueOf(640320)),
@@ -85,27 +86,12 @@ public class PiGenerator {
     }
 
     /**
-     * calculate the factorial of N by delegating to {@link #bigIntFactorial(BigInteger)}
-     * <p>
-     * This method takes an int so that it is an easier api to work with.
+     * calculate the factorial of N
      *
      * @param n the number to multiply by its previous positive numbers
      * @return product of all positive integers less than or equal to the input
      */
-    BigInteger factorial(int n) {
-        return bigIntFactorial(BigInteger.valueOf(n));
-    }
-
-    /**
-     * like {@link #factorial(int)} except takes a {@link BigInteger}
-     * <p>
-     * Because this method may get very large inputs,
-     * we can optionally use a {@link Map#computeIfAbsent(Object, Function)} cache.
-     *
-     * @param n a larger input
-     * @return product of all positive integers less than or equal to the input
-     */
-    BigInteger bigIntFactorial(BigInteger n) {
+    BigInteger factorial(BigInteger n) {
         if (!isUseCache())
             return doFactorial(n);
         return map.computeIfAbsent(n, this::doFactorial);
@@ -113,17 +99,17 @@ public class PiGenerator {
 
     /**
      * recursive function for computing a factorial.
-     * It calls {@link #bigIntFactorial(BigInteger)},
+     * It calls {@link #factorial(BigInteger)},
      * which means that the {@link Map} used must be safe to concurrently modify.
      *
      * @param n a larger input
-     * @return n multiplied by the previous positive number's {@link #bigIntFactorial(BigInteger)}
+     * @return n multiplied by the previous positive number's {@link #factorial(BigInteger)}
      */
     BigInteger doFactorial(BigInteger n) {
         if (n.equals(BigInteger.ZERO)) {
             return BigInteger.ONE;
         } else {
-            return n.multiply(bigIntFactorial(n.subtract(BigInteger.ONE)));
+            return n.multiply(factorial(n.subtract(BigInteger.ONE)));
         }
     }
 
