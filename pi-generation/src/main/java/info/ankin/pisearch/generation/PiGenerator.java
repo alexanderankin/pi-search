c2d99070f03406879124b5ea2c7478e9dc41a79b
David Ankin <daveankin@gmail.com>
2022-10-22 16:52:43 -0400

    add 500 digits test, 10i expected closer to pi now

--- pi-generation/src/main/java/info/ankin/pisearch/generation/PiGenerator.java
+++ pi-generation/src/main/java/info/ankin/pisearch/generation/PiGenerator.java
@@ -6,8 +6,10 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.MathContext;
 import java.math.RoundingMode;
+import java.util.Map;
 import java.util.NavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.function.Function;
 
 /**
  * @see <a href="https://stackoverflow.com/a/46166848">original answer on SO</a>
@@ -18,8 +20,18 @@ public class PiGenerator {
      */
     private static final int MAX_DECIMALS_IN_FLOAT = 16;
     private static final BigInteger NEGATIVE_ONE = BigInteger.ONE.negate();
-    private final NavigableMap<Integer, Integer> map;
+    /**
+     * Optional cache
+     */
+    private final NavigableMap<BigInteger, BigInteger> map;
+    /**
+     * {@link MathContext} which specifies precision for {@link BigDecimal} operations
+     */
     private final MathContext mathContext;
+    /**
+     * Configuration setting for whether {@link #map} is enabled - todo use null instead
+     */
+    private boolean useCache;
 
     public PiGenerator() {
         this(new ConcurrentSkipListMap<>(),
@@ -30,28 +42,37 @@ public class PiGenerator {
         this(new ConcurrentSkipListMap<>(), new MathContext(precision, RoundingMode.HALF_UP));
     }
 
-    public PiGenerator(NavigableMap<Integer, Integer> map, MathContext mathContext) {
+    public PiGenerator(NavigableMap<BigInteger, BigInteger> map, MathContext mathContext) {
         this.map = map;
         this.mathContext = mathContext;
     }
 
-    public double calculate(int k) {
-        double result = 0;
-        for (int i = 0; i <= k; i++) {
-            result = result + doCalc(i);
-        }
-        return 1 / (result * 12);
+    public boolean isUseCache() {
+        return useCache;
     }
 
-    double doCalc(int k) {
+    public PiGenerator setUseCache(boolean useCache) {
+        this.useCache = useCache;
+        return this;
+    }
+
+    public BigDecimal calculate(int k) {
+        BigDecimal result = BigDecimal.ZERO;
+        for (int i = 0; i <= k; i++) {
+            result = result.add(doCalc(i));
+        }
+        return BigDecimal.ONE.divide(result.multiply(BigDecimal.valueOf(12)), mathContext);
+    }
+
+    BigDecimal doCalc(int k) {
         BigInteger numerator = (k % 2 == 0 ? BigInteger.ONE : NEGATIVE_ONE)
-                .multiply(BigInteger.valueOf(factorial(6 * k)))
-                .multiply(BigInteger.valueOf((545140134L * k + 13591409)));
+                .multiply(factorial(6 * k))
+                .multiply(BigInteger.valueOf(545140134L * k + 13591409));
         BigDecimal d =
                 new BigDecimal(
                         factorial(BigInteger.valueOf(3).multiply(BigInteger.valueOf(k)).intValue())
                 )
-                        .multiply(BigDecimalMath.pow(new BigDecimal(BigInteger.valueOf(factorial(k))),
+                        .multiply(BigDecimalMath.pow(new BigDecimal(factorial(k)),
                                 new BigDecimal(BigInteger.valueOf(3)),
                                 mathContext))
                         .multiply(BigDecimalMath.pow(new BigDecimal(BigInteger.valueOf(640320)),
@@ -60,7 +81,7 @@ public class PiGenerator {
 
         //noinspection ConstantConditions
         if (2 < 1) {
-            double denominator = factorial(3 * k) * Math.pow(factorial(k), 3) * Math.pow(640320, 3 * k + 3.0 / 2.0);
+            double denominator = factorial(3 * k).longValue() * Math.pow(factorial(k).longValue(), 3) * Math.pow(640320, 3 * k + 3.0 / 2.0);
             if (d.doubleValue() != denominator) {
                 String plainDiff = new BigDecimal(d.doubleValue() - denominator).toPlainString();
                 String mantissa = mant(d.doubleValue()) + " vs " + mant(denominator);
@@ -73,26 +94,53 @@ public class PiGenerator {
         return
                 new BigDecimal(numerator)
                         // .divide(BigDecimal.valueOf(denominator), new MathContext(BigDecimal.valueOf(denominator).precision(), RoundingMode.HALF_UP)).doubleValue();
-                        .divide(d, new MathContext(d.precision(), RoundingMode.HALF_UP)).doubleValue();
+                        .divide(d, new MathContext(d.precision(), RoundingMode.HALF_UP));
     }
 
     private double mant(double denominator) {
         return denominator / Math.pow(2, Math.getExponent(denominator));
     }
 
-    int factorial(int n) {
-        return intFactorial(n);
+    /**
+     * calculate the factorial of N by delegating to {@link #bigIntFactorial(BigInteger)}
+     * <p>
+     * This method takes an int so that it is an easier api to work with.
+     *
+     * @param n the number to multiply by its previous positive numbers
+     * @return product of all positive integers less than or equal to the input
+     */
+    BigInteger factorial(int n) {
+        return bigIntFactorial(BigInteger.valueOf(n));
     }
 
-    int intFactorial(int n) {
+    /**
+     * like {@link #factorial(int)} except takes a {@link BigInteger}
+     * <p>
+     * Because this method may get very large inputs,
+     * we can optionally use a {@link Map#computeIfAbsent(Object, Function)} cache.
+     *
+     * @param n a larger input
+     * @return product of all positive integers less than or equal to the input
+     */
+    BigInteger bigIntFactorial(BigInteger n) {
+        if (!isUseCache())
+            return doFactorial(n);
         return map.computeIfAbsent(n, this::doFactorial);
     }
 
-    int doFactorial(int n) {
-        if (n == 0) {
-            return 1;
+    /**
+     * recursive function for computing a factorial.
+     * It calls {@link #bigIntFactorial(BigInteger)},
+     * which means that the {@link Map} used must be safe to concurrently modify.
+     *
+     * @param n a larger input
+     * @return n multiplied by the previous positive number's {@link #bigIntFactorial(BigInteger)}
+     */
+    BigInteger doFactorial(BigInteger n) {
+        if (n.equals(BigInteger.ZERO)) {
+            return BigInteger.ONE;
         } else {
-            return n * intFactorial(n - 1);
+            return n.multiply(bigIntFactorial(n.subtract(BigInteger.ONE)));
         }
     }
 
--- pi-generation/src/test/java/info/ankin/pisearch/generation/PiGeneratorTest.java
+++ pi-generation/src/test/java/info/ankin/pisearch/generation/PiGeneratorTest.java
@@ -3,6 +3,7 @@ package info.ankin.pisearch.generation;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class PiGeneratorTest {
 
@@ -11,7 +12,62 @@ class PiGeneratorTest {
         Object result = new PiGenerator().calculate(10);
         System.out.println(result);
 
-        assertEquals("3.1415926535897936", String.valueOf(result));
+        assertEquals("3.1415926535897932", String.valueOf(result));
+    }
+
+    @Test
+    void test_tenLines() {
+        logMemoryUsage();
+        String digits =
+                "3.14159265358979323846264338327950288419716939937510" +
+                        "58209749445923078164062862089986280348253421170679" +
+                        "82148086513282306647093844609550582231725359408128" +
+                        "48111745028410270193852110555964462294895493038196" +
+                        "44288109756659334461284756482337867831652712019091" +
+                        "45648566923460348610454326648213393607260249141273" +
+                        "72458700660631558817488152092096282925409171536436" +
+                        "78925903600113305305488204665213841469519415116094" +
+                        "33057270365759591953092186117381932611793105118548" +
+                        "07446237996274956735188575272489122793818301194912";
+        PiGenerator piGenerator = new PiGenerator(digits.length() - 1).setUseCache(false);
+
+        String myDigits = "";
+        int iteration;
+        for (iteration = 1; !myDigits.equals(digits); iteration++) {
+            myDigits = String.valueOf(piGenerator.calculate(iteration));
+            // System.out.println("doing iteration " + iteration + " and got " + myDigits.length() + " but needed: " + digits.length() + ". was: " + myDigits);
+
+            if (myDigits.length() == digits.length()) {
+                int similar = commonPrefixLength(digits, myDigits);
+                // System.out.println("right length, number similar: " + similar);
+
+                if (similar == digits.length() - 1) {
+                    break;
+                }
+            }
+
+            if (iteration > 40) fail("taking too many iterations");
+        }
+
+        // System.out.println("took " + iteration + " iterations to get to " + (digits.length() - 2) + " digits of pi");
+        logMemoryUsage();
+
+        // we expect it to take us this many iterations
+        assertEquals(35, iteration);
+    }
+
+    private void logMemoryUsage() {
+        System.out.printf("used %.3fMB%n", (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1_000_000.0);
+    }
+
+    private int commonPrefixLength(String expected, String actual) {
+        int i;
+        for (i = 0; i < expected.length(); i++) {
+            if (expected.charAt(i) != actual.charAt(i)) {
+                return i;
+            }
+        }
+        return i;
     }
 
 }
