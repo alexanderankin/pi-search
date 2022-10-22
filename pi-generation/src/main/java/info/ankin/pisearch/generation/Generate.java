934b5197860fd3b6663a5d3e50409467b0adce46
David Ankin <daveankin@gmail.com>
2022-10-22 17:27:51 -0400

    try running generate with large numbers

--- pi-generation/src/main/java/info/ankin/pisearch/generation/Generate.java
+++ pi-generation/src/main/java/info/ankin/pisearch/generation/Generate.java
@@ -2,14 +2,11 @@ package info.ankin.pisearch.generation;
 
 public class Generate {
     public static void main(String[] args) {
-        int toGenerate = 10;
-        /*
-        while (toGenerate-- > 0) {
-            int nextInt = 0;
-            System.out.println(nextInt);
-        }
-        */
-        PiGenerator piGenerator = new PiGenerator();
+        int precision = args.length > 0 ? Integer.parseInt(args[0], 10) : 20;
+        int toGenerate = args.length > 1 ? Integer.parseInt(args[1], 10) : 10;
+        System.err.println("printing PI with " + precision + " precision, after " + toGenerate + " iterations");
+        PiGenerator piGenerator = new PiGenerator(precision)
+                .setUseCache(false);
         System.out.println(piGenerator.calculate(toGenerate));
     }
 
