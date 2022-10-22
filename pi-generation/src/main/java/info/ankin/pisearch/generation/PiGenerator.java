8cd10898cf73343e6172340cf9a50ed5a84b8534
David Ankin <daveankin@gmail.com>
2022-10-22 13:19:18 -0400

    divide once, cache factorial answers

--- pi-generation/src/main/java/info/ankin/pisearch/generation/PiGenerator.java
+++ pi-generation/src/main/java/info/ankin/pisearch/generation/PiGenerator.java
@@ -1,28 +1,41 @@
 package info.ankin.pisearch.generation;
 
+import java.util.NavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+
 /**
  * @see <a href="https://stackoverflow.com/a/46166848">original answer on SO</a>
  */
 public class PiGenerator {
+    private final NavigableMap<Integer, Integer> map = new ConcurrentSkipListMap<>();
+
     public double calculate(int k) {
         double result = 0;
         for (int i = 0; i <= k; i++) {
             result = result + doCalc(i);
         }
-        return 1 / result;
+        return 1 / (result * 12);
     }
 
     double doCalc(int k) {
         double numerator = Math.pow(-1, k) * factorial(6 * k) * (545140134 * k + 13591409);
         double denominator = factorial(3 * k) * Math.pow(factorial(k), 3) * Math.pow(640320, 3 * k + 3.0 / 2.0);
-        return 12.0 * numerator / denominator;
+        return numerator / denominator;
     }
 
     double factorial(int n) {
+        return intFactorial(n);
+    }
+
+    int intFactorial(int n) {
+        return map.computeIfAbsent(n, this::doFactorial);
+    }
+
+    int doFactorial(int n) {
         if (n == 0) {
             return 1;
         } else {
-            return n * factorial(n - 1);
+            return n * intFactorial(n - 1);
         }
     }
 
